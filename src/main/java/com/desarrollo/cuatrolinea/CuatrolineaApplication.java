db3fa1cbd086df6c5c6b56d9590c605a62f982b3
Nestor Marsollier <nmarsollier@gmail.com>
2023-06-08 11:05:43 -0300

    Update CuatrolineaApplication.java

--- src/main/java/com/desarrollo/cuatrolinea/CuatrolineaApplication.java
+++ src/main/java/com/desarrollo/cuatrolinea/CuatrolineaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CuatrolineaApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CuatrolineaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CuatrolineaApplication.class, args);
+    }
 }
